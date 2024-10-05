82a262c1ec4b89e85cc927346fb70a3deedc637a
Syuhada Rantisi <suadarantisi@gmail.com>
2024-10-05 16:07:40 +0700

    Add BackButton in Lobby - client cabut belum bisa ilang PP nya

--- Assets/Scripts/Manager/LobbyManager.cs
+++ Assets/Scripts/Manager/LobbyManager.cs
@@ -8,25 +8,35 @@ using UnityEngine.UI;
 
 public class LobbyManager : NetworkBehaviour
 {
+    private List<string> playerNames = new();
     public Image[] playerProfile;
     public Sprite[] newPlayerProfile;
     public TextMeshProUGUI[] playerName;
     public Button startButton;
+    public Button backButton;
 
     void Start()
     {
         Debug.Log("LobbyManager Active");
         startButton.onClick.AddListener(OnStartButtonPressed);
+        backButton.onClick.AddListener(OnBackButtonPressed);
         NetworkManager.Singleton.OnClientConnectedCallback += OnPlayerJoined;
+        NetworkManager.Singleton.OnClientDisconnectCallback += OnPlayerLeave;
     }
 
     // Callback saat pemain bergabung
     private void OnPlayerJoined(ulong clientId)
     {
         Debug.Log($"{DataPersistence.LoadUsername()}");
-        Debug.Log("Player Joined dengan Client ID: " + clientId);
+        // Debug.Log("Player Joined dengan Client ID: " + clientId);
         SendProfileToAllClientRpc(clientId);
         // SendPlayerNameServerRpc();
+        NotifyPlayerJoinClientRpc(clientId);
+    }
+
+    private void OnPlayerLeave(ulong clientId)
+    {
+        NotifyPlayerLeaveClientRpc(clientId);
     }
 
     // [ServerRpc(RequireOwnership = false)]
@@ -51,7 +61,7 @@ public class LobbyManager : NetworkBehaviour
         int totalPlayer = (int)clientId;
         for (int i = 0; i <= totalPlayer; i++)
         {
-            playerName[i].text = $"Player {i}";
+            playerName[i].text = $"Player {i + 1}";
             if (playerProfile[i] != null)
             {
                 playerProfile[i].sprite = newPlayerProfile[i];
@@ -85,13 +95,34 @@ public class LobbyManager : NetworkBehaviour
         if (NetworkManager.Singleton != null && NetworkManager.Singleton.IsHost)
         {
             NetworkManager.Singleton.Shutdown();
-            Debug.Log("NetworkManager shut down successfully.");
+            Debug.Log("NetworkManager in Host shut down successfully.");
+            ReturnToMainMenuClientRpc();
         }
-
-        // Kembali ke Main Menu (implementasi LoadScene sesuai dengan logika kamu)
-        SceneManager.LoadScene("MainMenu");
-
+        else if (NetworkManager.Singleton != null && NetworkManager.Singleton.IsClient)
+        {
+            NetworkManager.Singleton.Shutdown();
+            Debug.Log("NetworkManager in Client shut down successfully.");
+            SceneManager.LoadScene("MainMenu");
+        }
     }
 
+    // Server membuat semua client berpindah ke MainMenu
+    [ClientRpc]
+    private void ReturnToMainMenuClientRpc()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    [ClientRpc]
+    private void NotifyPlayerJoinClientRpc(ulong clientId)
+    {
+        Debug.Log($"JOIN Player ID: {clientId}");
+    }
+
+    [ClientRpc]
+    private void NotifyPlayerLeaveClientRpc(ulong clientId)
+    {
+        Debug.Log($"LEAVE Player ID: {clientId}");
+    }
 
 }
