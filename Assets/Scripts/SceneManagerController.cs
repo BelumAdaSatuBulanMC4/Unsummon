eaf8d64e51b377f0d56af8069ccd26426b14e168
Syuhada Rantisi <suadarantisi@gmail.com>
2024-11-18 13:51:33 +0700

    Fixing story game in first time user

--- Assets/Scripts/SceneManagerController.cs
+++ Assets/Scripts/SceneManagerController.cs
@@ -3,28 +3,28 @@ using UnityEngine.SceneManagement;
 
 public class SceneManagerController : MonoBehaviour
 {
-    private const string FirstTimeKey = "FirstTimeUser"; // Key untuk menyimpan status first-time user
 
     void Start()
     {
         // Cek apakah user pertama kali menggunakan game
         if (IsFirstTimeUser())
         {
+            Debug.Log("Start - Sekarang user frist time ");
             // Jika pertama kali, pindah ke GameStory scene
             SceneManager.LoadScene("GameStory");
         }
         else
         {
-            // Jika bukan pertama kali, pindah ke MainMenu scene
-            SceneManager.LoadScene("MainMenu");
+            Debug.Log("Start - Sekarang user non frist time ");
         }
     }
 
     // Fungsi untuk mengecek apakah user pertama kali menggunakan game
+
     private bool IsFirstTimeUser()
     {
         // Cek apakah PlayerPrefs sudah ada untuk first-time key
-        if (PlayerPrefs.HasKey(FirstTimeKey))
+        if (PlayerPrefs.HasKey("IsFirstTime"))
         {
             // Jika sudah, berarti ini bukan pertama kali
             return false;
@@ -32,7 +32,7 @@ public class SceneManagerController : MonoBehaviour
         else
         {
             // Jika tidak ada, berarti pertama kali dan tandai user sudah memainkan game
-            PlayerPrefs.SetInt(FirstTimeKey, 1);  // Menandai bahwa user sudah pertama kali menggunakan game
+            PlayerPrefs.SetInt("IsFirstTime", 1);  // Menandai bahwa user sudah pertama kali menggunakan game
             PlayerPrefs.Save();  // Menyimpan perubahan
             return true;
         }
